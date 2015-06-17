6543fde7e094004fc57452d096e5e2c03752d09b
Faisal Ali <siddee.faisalali@gmail.com>
2015-06-17 14:54:18 -0400

    changes based on feedback from reviewer, added button styles and removed menu

--- app/src/main/java/com/sideez/mynanodegreeportfolio/PortfolioActivity.java
+++ app/src/main/java/com/sideez/mynanodegreeportfolio/PortfolioActivity.java
@@ -7,8 +7,6 @@ package com.sideez.mynanodegreeportfolio;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,28 +17,6 @@ public class PortfolioActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_portfolio, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /* Called when the user clicks the button */
